2fa63a1cec1733e69d5a1457e770833756b52fb5
Alex Junior <alexjunior2018@gmail.com>
2024-12-02 13:37:53 -0300

    adicionando secao

--- JavaEssencial/src/br/com/alexjr/secao10/Conta.java
+++ JavaEssencial/src/br/com/alexjr/secao10/Conta.java
@@ -1,23 +1,47 @@
 package br.com.alexjr.secao10;
 
 public class Conta {
-	int numero;
-	float saldo;
-	float limite;
-	Cliente cliente;
+    private int numero;
+    private float saldo;
+    private float limite;
+    private Cliente cliente;
 
-	public Conta(int numero, float saldo, float limite, Cliente cliente) {
-		this.numero = numero;
-		this.saldo = saldo + limite;
-		this.limite = limite;
-		this.cliente = cliente;
-	}
+    public Conta(int numero, float saldo, float limite, Cliente cliente) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.limite = limite;
+        this.cliente = cliente;
+    }
 
-	void sacar(float valor) {
-		this.saldo = this.saldo - valor;
-	}
+    public void sacar(float valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+        } else if (valor <= (this.saldo + this.limite)) {
+            float excedente = valor - this.saldo;
+            this.saldo = 0;
+            this.limite -= excedente;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
 
-	void depositar(float valor) {
-		this.saldo = this.saldo + valor;
-	}
+    public void depositar(float valor) {
+        this.saldo += valor;
+    }
+
+    public float getSaldo() {
+        return this.saldo + this.limite;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
--- JavaEssencial/src/br/com/alexjr/secao10/Programa25.java
+++ JavaEssencial/src/br/com/alexjr/secao10/Programa25.java
@@ -1,20 +1,21 @@
 package br.com.alexjr.secao10;
 
+//Encapsulamento
+//Capsula
 public class Programa25 {
 
-	public static void main(String[] args) {
-		Cliente joao = new Cliente("João da Silva", "Rua da paz, 45");
-		Cliente maria = new Cliente("Maria da Silva", "Rua da paz, 45");
+    public static void main(String[] args) {
+        Cliente joao = new Cliente("João da Silva", "Rua da paz, 45");
+        Cliente maria = new Cliente("Maria da Silva", "Rua da paz, 45");
 
-		Conta contaJoao = new Conta(1, 100.0f, 200.0f, joao);
-		Conta contaMaria = new Conta(2, 300.0f, 500.0f, maria);
+        Conta contaJoao = new Conta(1, 100.0f, 200.0f, joao);
+        Conta contaMaria = new Conta(2, 300.0f, 500.0f, maria);
 
-		System.out.println("Saldo do João (Antes do Saque): " + contaJoao.saldo);
-		System.out.println("Saldo da Maria: " + contaMaria.saldo);
-		
-		contaJoao.sacar(300);
-		System.out.println("Saldo do João (Depois do Saque): " + contaJoao.saldo);
+        System.out.println("Saldo do João (Antes do Saque): " + contaJoao.getSaldo());
+        System.out.println("Saldo da Maria: " + contaMaria.getSaldo());
 
-	}
-
+        contaJoao.sacar(150);
+        System.out.println("Saldo do João (Depois do Saque): " + contaJoao.getSaldo());
+    }
 }
+
