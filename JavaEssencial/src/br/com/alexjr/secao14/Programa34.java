1c48b43c20a78ccef5cc27f7ab56c41587b48c35
Alex Jr <alexjunior2018@gmail.com>
2024-12-05 08:57:01 -0300

    adicionando exercicio

---
+++ JavaEssencial/src/br/com/alexjr/secao14/Programa34.java
@@ -0,0 +1,13 @@
+package br.com.alexjr.secao14;
+
+//Atributos estáticos
+
+/*
+ * Atributos Estáticos são atributos, onde os valores são compartilhados
+ * entre as instâncias da
+ */
+public class Programa34 {
+	public static void main(String[] args) {
+
+	}
+}
